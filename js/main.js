38706d6b11a368e551c9b6bffc6b1be6afec3275
MessaoudiHoussam <houssmnadia1995@gmail.com>
2020-02-14 21:00:57 +0100

    maptoken

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWVzc2FvdWRpaCIsImEiOiJjazZta2d3Zm0wMWlzM2txcHNtaGZjNWd2In0.x6eVN-ljFKuQK1-587f7Bw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
